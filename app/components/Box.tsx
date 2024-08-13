d3a319e44a50161faf4a41d03a3972156d9c9bb7
AyushShivhare79 <ayushshivhare1008@gmail.com>
2024-08-13 10:32:43 +0530

    chore: updates

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -1,20 +1,29 @@
 "use client";
 import Typewriter from "typewriter-effect";
 import SocialIcon from "./SocialIcon";
+import { Button } from "@/components/ui/button";
 
 export default function Box() {
   return (
     <>
-      <div className="flex justify-center flex-col items-center gap-5 font-semibold border border-white text-7xl font-serif h-[700px]">
-        <h1>Ayush Shivhare</h1>
-        <Typewriter
-          options={{
-            strings: ["Full Stack Developer"],
-            autoStart: true,
-            loop: true,
-          }}
-        />
+      <div className="flex justify-center flex-col items-center gap-5 border border-white h-[700px]">
+        <div className="flex justify-center flex-col items-center font-semibold text-7xl font-serif">
+          <h1>Ayush Shivhare</h1>
+          <Typewriter
+            options={{
+              strings: ["Full Stack Developer"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
         <SocialIcon />
+        <div className="flex gap-5">
+          <Button className="bg-white text-black text-base">
+            Download My Resume
+          </Button>
+          <Button className="bg-white text-black text-base">Contact Me</Button>
+        </div>
       </div>
     </>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 import Navbar from "./components/Navbar";
 import Box from "./components/Box";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Box />
-      <About />
+      <Projects />
+      {/* <About /> */}
     </>
   );
 }
